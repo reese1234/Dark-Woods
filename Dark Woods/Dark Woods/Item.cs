c92bc613cd2d84da8afadcb6e1dcc284017374c8
Reese Lingad <reeselingad@gmail.com>
2020-04-16 09:47:51 +1200

    Initial Commit

---
+++ Dark Woods/Dark Woods/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dark_Woods
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Effect { get; set; }
+        public int EffectValue { get; set; }
+    }
+}
---
+++ Dark Woods/Dark Woods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Dark_Woods
+{
+    class Program
+    {
+        public static List<Item> Items = new List<Item>();
+        static void Main()
+        {
+            for (; ; )
+            {
+                try
+                {
+                    Play();
+                } catch (Exception e)
+                {
+                    Message("Oh no! An error occurred!", ConsoleColor.Red);
+                    Console.WriteLine(e);
+                } finally
+                {
+                    Message("Restarting...", ConsoleColor.Green);
+                    Thread.Sleep(2000);
+                }
+            }
+        }
+
+        public static void Play()
+        {
+            new Item
+            {
+                Name = "Twig",
+                Description = "A random twig you found."
+            };
+            throw new Exception();
+        }
+
+        public static void Message(string say, ConsoleColor color) //Move and fix this later
+        {
+            Console.ForegroundColor = color;
+            foreach (char letter in say)
+            {
+                Console.Write(letter);
+                Thread.Sleep(50);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        }
+    }
+}
